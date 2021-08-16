bfced6842eba51477e884ed1ec3a82b3f5ed2802
zeeshanqadir <z.qadir@iitkgp.ac.in>
2021-08-16 13:32:56 -0500

    second subject added

--- code/preprocessing.m
+++ code/preprocessing.m
@@ -1,14 +1,15 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% SETUP
 
+%path D:\Zeeshan\reading-ecog\code
 
 % define
-subjects = {'YBA'};
-subjectfilenums = {[12 13 14 15]};  % this is for the file names (one file per run per [subject])
+subjects = {'YBA' 'YBD'};
+subjectfilenums = {[12 13 14 15] [30 31 32 33]};  % this is for the file names (one file per run per [subject])
 channels = 1:128;
-numchannels = 96;   % only for subject YBA; when including other subjects make 128;
+numchannels = 128;
 photodiode = 129;   % photodiode signal is in #129
 epochrng = [-.5 3.5];  % pull out this time range (in seconds)
-onsetix = {{1:66 1:66 1:66 1:66}};  % indices to pull the black circle (based on the no of stimulus trials per run; photodiode detects this circle)
+onsetix = {{1:66 1:66 1:66 1:66} {1:66 1:66 1:66 1:66}};  % indices to pull the black circle (based on the no of stimulus trials per run; photodiode detects this circle)
 fsorig = 2000;    % sampling rate (original)
 fsjump = 10;      % moving average length
 fs = fsorig/fsjump;         		% sampling rate (down-sampled to)
@@ -84,19 +85,23 @@ repmat({'X'},[1 32]) ...
 onsets = zeros(numtrials,length(subjects),numruns);  % indices of data points at which trials occur
 data = zeros(length(epochtime),length(subjects),numchannels,numtasks,numreps,numstimuli,'single');
 bb_data = zeros(length(epochtime_bb),length(subjects),numchannels,numtasks,numreps,numstimuli,'single');
+nb_data = zeros(length(epochtime_bb),length(subjects),numchannels,numtasks,numreps,numstimuli,'single');
+alpha_data = zeros(length(epochtime_bb),length(subjects),numchannels,numtasks,numreps,numstimuli,'single');
+beta_data = zeros(length(epochtime_bb),length(subjects),numchannels,numtasks,numreps,numstimuli,'single');
 psd_on = zeros(length(subjects),numchannels,numtasks,numreps,numstimuli,fupper,'single');
 psd_off = zeros(length(subjects),numchannels,numtasks,numreps,numstimuli,fupper,'single');
 stimcounter = zeros(length(subjects),numchannels,numtasks,numstimuli);  % number of trials encountered so far
 recdata = {};
 bbtemp = {};
-bb_bptemp = {};
+onsetpoints = {};
+
 for zzz=1:length(subjects)
 
   % get behavioral files
   files = matchfiles(sprintf('../data/%s/LogFiles/*.mat',subjects{zzz}))   %matchfiles: to match the filename (knkutils)
   assert(length(files)==length(subjectfilenums{zzz}));
   
-  onsetpoints = []; % done only for YBA. need to revisit while including other subjects
+  temponsetpoints = []; 
   optemp = 0;
   % process each run (photodiode)
   for p=1:length(subjectfilenums{zzz})
@@ -109,7 +114,7 @@ for zzz=1:length(subjects)
     fs0 = pd.analogInfos.SampleRate; assert(fs0==fsorig);
     
     % get good time region (rough pass)
-    numsam = length(pd.analogTraces); 
+    numsam = length(pd.analogTraces);
     [~,theseOnsets] = findpeaks(-pd.analogTraces,'MinPeakDistance',3*fsorig,'MinPeakHeight',150);  % if black is low, then - means find pos deflections
     theseOnsets = theseOnsets(onsetix{zzz}{p});  % this is not quite right (finding peaks), but roughly right
     
@@ -123,16 +128,17 @@ for zzz=1:length(subjects)
 
     % visualize for sanity
     %figureprep([100 100 1000 300]); hold on;		% figureprep & figurewrite(knkutils)
-    figure('position',[100 100 1000 300]); hold on;
+    figureprep([100 100 1000 300]); hold on;
     plot(pd.analogTraces);
     straightline(theseOnsets,'v','m-');
     figurewrite(sprintf('photodiode_subj%d_file%d',zzz,p),[],[],'~/inout/photodiode');
 
     % record
     onsets(:,zzz,p) = theseOnsets;
-    onsetpoints = [onsetpoints optemp+theseOnsets]; % cumulative epoch points
+    temponsetpoints = [temponsetpoints optemp+theseOnsets]; % cumulative epoch points
     optemp = optemp + length(pd.analogTraces);	% cumulative length of runs 
   end
+  onsetpoints{zzz} = single(temponsetpoints);
   
   % process each channel
   for ccc=1:numchannels
@@ -169,39 +175,50 @@ for zzz=1:length(subjects)
 	% OLD way (version 1): bb = g(pd.analogTraces);   % <===== NOTE THIS!!! 
 
     collectdata = collectdata';  %convert to time X channels
+   
+    % TOTAL HACK SPIKE (detect later!)
+    if zzz==1   %subject YBA
+      badrng = 1.072 * 10^6 : 1.09 * 10^6; % data seems to be corrupted in this time range for all channels
+      %bb(badrng) = NaN;  %%median(bb(setdiff(1:length(bb),badrng)));
+      collectdata(badrng) = nanmedian(collectdata);
+    end
+    
 	% NEW way:
-    bands = [70 90;  % 20 hz bins, avoiding 60 and 180 (max 210)
-             90 110;
+    bands = [70  90;  % 20 hz bins, avoiding 60 and 180 (max 210)
+             90  110;
              110 130;
              130 150;
              150 170];   %% HACK OUT
 %             190 210];
-    bb = ieeg_getHilbert(collectdata,{[70 90] [90 110] [130 150] [150 170]}, fsorig, 'power');  % NOTE: WE DO WHOLE EXPERIMENT AT ONCE TO ENSURE EQUAL SCALING ISSUE...
+    narrowband = [30 50];  
+    beta  = [12 30];
+    alpha = [8 12];
+    theta = [4 7];
+    delta = [0 4];
+    
+    bb = ecog_extractBroadband(collectdata,fsorig,[],bands);  % NOTE: WE DO WHOLE EXPERIMENT AT ONCE TO ENSURE EQUAL SCALING ISSUE...
                                                               % ecog_extractBroadband: mean after hilbert; power; geomean (ECoG utilities by JW_NYU)
- 
-    bb_bp = ieeg_butterpass(collectdata, [70 170], fsorig);   % bandpass; amplitude (mnl_ieegBasics by DH)
+    
+    nb = ecog_extractBroadband(collectdata,fsorig,[],narrowband); 
+    betab  = ecog_extractBroadband(collectdata,fsorig,[],beta);
+    alphab = ecog_extractBroadband(collectdata,fsorig,[],alpha); 
+    
+    %bb_bp = ieeg_butterpass(collectdata, [70 170], fsorig);   % bandpass; amplitude (mnl_ieegBasics by DH)
 
 	% 	RAW CASE: bb = pd.analogTraces;
 
     
     bbtemp{zzz,ccc} = single(bb);   % save broadband so we can inspect it!
-    bb_bptemp{zzz,ccc} = single(bb_bp);
-    
-
-    % TOTAL HACK SPIKE (detect later!)
-    assert(all(isfinite(bb)));
-    if zzz==1   %subject YBA
-      badrng = 1.072 * 10^6 : 1.09 * 10^6; % data seems to be corrupted in this time range for all channels
-      bb(badrng) = NaN;  %%median(bb(setdiff(1:length(bb),badrng)));
-      collectdata(badrng) = NaN;
-      badval = nanmedian(bb);  % **ROBUST
-      badvalraw = nanmedian(collectdata);
-    end
-    
     recdata{zzz,ccc} = single(collectdata);    % raw data amplitude
+
+    
+    %assert(all(isfinite(bb)));
+    
     % compute moving average to reduce data rate to 200Hz (2000/10)
     bb = movmean(bb,  fsjump,1);  % slight slop due to evenness of the number.  we can plop this in the photodiode transition issue.
-    bb_bp = movmean(bb_bp,  fsjump,1);
+    nb = movmean(nb,  fsjump,1);
+    betab = movmean(betab,  fsjump,1);
+    alphab = movmean(alphab,  fsjump,1);
     
     % process each run
     for p=1:length(subjectfilenums{zzz})
@@ -217,30 +234,37 @@ for zzz=1:length(subjects)
         %off_samples = cat (2, 1 : length(epochrng(1)*fsorig:0*fsorig)-1, length(epochrng(1)*fsorig:0*fsorig) + 2 * fsorig : 4*fsorig);  % samples corresponding to off-stimuli-period
         off_samples = 1 : length(epochrng(1)*fsorig:0*fsorig)-1;
         temp_bb = bb(sum(bblengths(1:p-1)) + ix_bb);			% bb analysis
-		temp_raw = collectdata(sum(bblengths(1:p-1)) + ix_raw);	% raw analysis
-        if any(isnan(temp_bb))
-          fprintf('BADDATA: ttt=%d, p=%d, ccc=%d, zzz=%d\n',ttt,p,ccc,zzz);
-          temp_bb(:) = badval;
-        end
-        if any(isnan(temp_raw))
-		  temp_raw(:) = badval;
-        end
+        temp_nb = nb(sum(bblengths(1:p-1)) + ix_bb);            % nb analysis
+        temp_beta = betab(sum(bblengths(1:p-1)) + ix_bb);            % beta band analysis
+        temp_alpha = alphab(sum(bblengths(1:p-1)) + ix_bb);            % alpha band analysis
         
-		%stimco = stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1;
-        bb_data(:,zzz,ccc,mod2(p,2), ...
-             stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1, ...   % a1.stimclassrec tells us the stim number (1-24)
+		temp_raw = collectdata(sum(bblengths(1:p-1)) + ix_raw);	% raw analysis
+%         if any(isnan(temp_bb))
+%           fprintf('BADDATA: ttt=%d, p=%d, ccc=%d, zzz=%d\n',ttt,p,ccc,zzz);
+%           temp_bb(:) = badval;
+%         end
+%         if any(isnan(temp_raw))
+% 		  temp_raw(:) = badval;
+%         end
+        
+		stimco = stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1;   % a1.stimclassrec tells us the stim number (1-24)
+        
+        bb_data(:,zzz,ccc,mod2(p,2), stimco, ...
              a1.stimclassrec(ttt)) = temp_bb;
-        data(:,zzz,ccc,mod2(p,2), ...
-             stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1, ...   % a1.stimclassrec tells us the stim number (1-24)
-            a1.stimclassrec(ttt)) = temp_raw;
+        nb_data(:,zzz,ccc,mod2(p,2), stimco, ...
+             a1.stimclassrec(ttt)) = temp_nb;
+        beta_data(:,zzz,ccc,mod2(p,2), stimco, ...
+             a1.stimclassrec(ttt)) = temp_beta;
+        alpha_data(:,zzz,ccc,mod2(p,2), stimco, ...
+             a1.stimclassrec(ttt)) = temp_alpha;
+        data(:,zzz,ccc,mod2(p,2), stimco, ...
+             a1.stimclassrec(ttt)) = temp_raw;
         [psdvar,f] = pwelch(temp_raw(on_samples)',hamming(1000),0,2 ^ nextpow2(fsorig),fsorig);
          %figureprep([100 100 900 300],1);plot(f,10*log10(psdvar));
-        psd_on(zzz,ccc,mod2(p,2), ...
-             stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1, ...   % calculating psd from raw data
+        psd_on(zzz,ccc,mod2(p,2), stimco, ...
              a1.stimclassrec(ttt),:) = psdvar(1:fupper);
         [psdvar,f] = pwelch(temp_raw(off_samples)',hamming(1000),0,2 ^ nextpow2(fsorig),fsorig);
-        psd_off(zzz,ccc,mod2(p,2), ...
-             stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1, ...   % calculating psd from raw data
+        psd_off(zzz,ccc,mod2(p,2), stimco, ...
              a1.stimclassrec(ttt),:) = psdvar(1:fupper);
 		stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) = ...
           stimcounter(zzz,ccc,mod2(p,2),a1.stimclassrec(ttt)) + 1;
@@ -252,15 +276,18 @@ end
 
 %% CHANNEL AVG TIME AND FREQUENCY RESPONSE
 
-psd_baseline = squeeze(mean(reshape(psd_off,zzz*numchannels*numtasks*numreps*numstimuli,fupper),1));
-bb_data_avg = mean(reshape(bb_data,length(epochtime_bb),numchannels,numtasks*numreps*numstimuli),3)';
-data_avg = mean(reshape(data,length(epochtime),numchannels,numtasks*numreps*numstimuli),3)';
-psd_avg = squeeze(mean(reshape(psd_on,numchannels,numtasks*numreps*numstimuli,fupper),2));
+psd_baseline = squeeze(mean(reshape(psd_off,length(subjects)*numchannels*numtasks*numreps*numstimuli,fupper),1));
+bb_data_avg = mean(reshape(bb_data,length(epochtime_bb),length(subjects)*numchannels,numtasks*numreps*numstimuli),3)';
+nb_data_avg = mean(reshape(nb_data,length(epochtime_bb),length(subjects)*numchannels,numtasks*numreps*numstimuli),3)';
+alpha_data_avg = mean(reshape(alpha_data,length(epochtime_bb),length(subjects)*numchannels,numtasks*numreps*numstimuli),3)';
+beta_data_avg = mean(reshape(beta_data,length(epochtime_bb),length(subjects)*numchannels,numtasks*numreps*numstimuli),3)';
+data_avg = mean(reshape(data,length(epochtime),numchannels,length(subjects)*numtasks*numreps*numstimuli),3)';
+psd_avg = squeeze(mean(reshape(psd_on,numchannels,length(subjects)*numtasks*numreps*numstimuli,fupper),2));
 
 %% PLOT CHANNEL AVG TIME AND FREQUENCY RESPONSE
 
 clims = [0 50];
-figure,imagesc(bb_data_avg, [0, 1e25]);
+figure,imagesc(bb_data_avg,[0 50]);
 colorbar;
 set(gca,'XTick',0:100:800)
 set(gca,'XTickLabel',-0.5:0.5:3.5)
@@ -279,85 +306,75 @@ hold off;
 
 %% Plot normalized PSD
 
-ccc = 27;
+ccc = 75;
 plot((normalize(10*log10(psd_avg(ccc,:)) - 10*log10(psd_baseline))))
+
+%% Normalize Data
+
+
 
 %% Stimuli Timecourse
 
-ccc = 75;
+stimgroups  = {[6 7 8 9 4]   [10 11 12 13 5] [14 15 16 4] [17 18 19 5]};% [20 21 22 10] [2 23 24]};
+stimgrnames = {'Word Phase' 'Face Phase'    'Word Con'   'Face Con'};%   'Noise Con'   'Other'};
 
-subplot(2,4,1)
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,6),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,7),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,8),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,9),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,4),5))));
-hold off;
-title('WP_F');
+for zzz = 1:length(subjects)
+   
+    for ccc = 1:numchannels
+        
+        figureprep([100 100 1700 1100]);
+    
+        for stimg = 1:length(stimgroups)
+            
+            subplot(2,length(stimgroups),stimg)
+            for stimc = 1:length(stimgroups{stimg})
+                
+                plot(smooth(squeeze(mean(bb_data(:,zzz,ccc,1,:,stimgroups{stimg}(stimc)),5)),40));
+                %ylim([-50 350]); 
+                xlim([0 800]);
+                set(gca,'XTick',0:100:800); set(gca,'XTickLabel',-0.5:0.5:3.5);
+                hold on;
+                
+            end
+            hold off;
+            title(sprintf('%s_F',stimgrnames{stimg}));
+            
+            subplot(2,length(stimgroups),length(stimgroups)+stimg)
+            for stimc = 1:length(stimgroups{stimg})
+                
+                plot(smooth(squeeze(mean(bb_data(:,zzz,ccc,2,:,stimgroups{stimg}(stimc)),5)),40));
+                %ylim([-50 350]); 
+                xlim([0 800]);
+                set(gca,'XTick',0:100:800); set(gca,'XTickLabel',-0.5:0.5:3.5);
+                hold on;
+                
+            end
+            hold off;
+            title(sprintf('%s_C',stimgrnames{stimg}));
+            
+        end
+        figurewrite(sprintf('Subj%d_ch%03d',zzz,ccc),[],[],'stimtimecourse');
+        
+    end
+    
+end
 
-subplot(2,4,5)
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,6),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,7),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,8),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,9),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,4),5))));
-hold off;
-title('WP_C');
 
-subplot(2,4,2)
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,10),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,11),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,12),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,13),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,5),5))));
-hold off;
-title('FP_F');
 
-subplot(2,4,6)
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,10),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,11),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,12),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,13),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,5),5))));
-hold off;
-title('FP_C');
 
-subplot(2,4,3)
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,14),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,15),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,16),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,4),5))));
-hold off;
-title('WC_F');
 
-subplot(2,4,7)
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,14),5))));
-ylim([-50 350]); xlim([0 800]); hold on;  
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,15),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,16),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,4),5))));
-hold off;
-title('WC_C');
+%%
+% what we did:
+% - did a moving average of size 10 (to go from 2000 to 200)
+% - for each of the 66 trials, define time=0 based on the photodiode zero-crossing (leftward).
+%   then extract the epoch range [-.5 3.5]*fs from the broadband time-series and save it.
+%   note that these epochs no longer overlap a bit!
+%
+% outputs:
+% - <data> is epochtime x 3 subjects x 128 channels x 2 tasks x 6 trials x 24 stimuli
+%   - note that the chronological order of the trials is preserved.
+%   - note that stimulus #1 and #3 are not presented, so the data for these will be zeros.
+%
+% NOTICE HOW WE COMPUTE BB ON THE ENTIRE DATASET AT ONCE!  otherwise, you'll get weird run to run differences.
 
-subplot(2,4,4)
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,17),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,18),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,19),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,1,:,5),5))));
-hold off;
-title('FC_F');
 
-subplot(2,4,8)
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,17),5))));
-ylim([-50 350]); xlim([0 800]); hold on; 
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,18),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,19),5))));
-plot((squeeze(mean(bb_data(:,1,ccc,2,:,5),5))));
-hold off;
-title('FC_C');
