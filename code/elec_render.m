375c0e83d24d470266519e28947e79d02993c187
zeeshanqadir <qadir.zeeshan@mayo.edu>
2022-05-20 16:34:47 -0500

    Brain rendering of electrodesupdated

--- code/elec_render.m
+++ code/elec_render.m
@@ -12,15 +12,15 @@ FSsubjectsdir = '/Applications/freesurfer/7.2.0/subjects/';
  
 figure
 % transform to gifti
-gl.faces = Lmnipial_face+1;
-gl.vertices = Lmnipial_vert;
+gl.faces = Rmnipial_face+1;
+gl.vertices = Rmnipial_vert;
 gl = gifti(gl);
  
 % render
 tH = ieeg_RenderGifti(gl);
 
 % rotate with light
-ieeg_viewLight(-180, -90)
+ieeg_viewLight(90, 0)
  
 %% YBA Sub-01
 
@@ -42,59 +42,59 @@ ieeg_elAdd([RPTO1; RPTO2; RPTO3; RPTO4; RPTO5; RPTO6; RPTO7; RPTO8] ,'y',30)
 % RP 1-6, RF 1-6
 % ieeg_viewLight(90, 0) RL
 
-RF1 = [40.6, 6.5, 59.3];
-RF2 = [55.9, 1.4, 45.7];
-RF3 = [60.8, -1.8, 34.2];
-RF4 = [66.8, -7.7, 26];
-RF5 = [68.9, -13.6, 17.5];
+RF1 = [40.6, -3.8, 59.3];
+RF2 = [55.9, -3.8, 46.7];
+RF3 = [57.8, -6.4, 32.5];
+RF4 = [67.8, -10.5, 22.7];
+RF5 = [68.9, -15.6, 14.5];
 RF6 = [69.7, -21, 4.6];
 
-ieeg_elAdd([RF1; RF2; RF3; RF4; RF5; RF6] ,'y',30)
+ieeg_elAdd([RF1; RF2; RF3; RF4; RF5; RF6] ,'b',30)
 
 RP1 = [39.3, -44.5, 57.6];
 RP2 = [58, -37.8, 48];
 RP3 = [63.5, -34.6, 38];
-RP4 = [65, -35.6, 20];
+RP4 = [65, -32.6, 23];
 RP5 = [69, -29.8, 10.7];
 RP6 = [70.2, -29, 2.6];
 
-ieeg_elAdd([RP1; RP2; RP3; RP4; RP5; RP6] ,'y',30)
+ieeg_elAdd([RP1; RP2; RP3; RP4; RP5; RP6] ,'g',30)
 
 % RAIT 1-4, RMIT 1-4, RPIT 1-4
 % ieeg_viewLight(-90, -90) RI
 
-RAIT1 = [24.5, 7.5, -45];
-RAIT2 = [34.3, 4.4, -45.2];
-RAIT3 = [41.7, 0, -46];
-RAIT4 = [48.2, -5.9, -44.5]; 
+RAIT1 = [24.5, 7.5, -44.5];
+RAIT2 = [36.7, 3.6, -45];
+RAIT3 = [46.7, -3.5, -45.5];
+RAIT4 = [52.6, -11.3, -43]; 
 
-ieeg_elAdd([RAIT1; RAIT2; RAIT3; RAIT4] ,'y',30)
+ieeg_elAdd([RAIT1; RAIT2; RAIT3; RAIT4] ,'g',30)
 
-RMIT1 = [32.5, -44.9, -23]; 
-RMIT2 = [39.1, -44.6, -24]; 
-RMIT3 = [48.4, -44.4, -21.5];
-RMIT4 = [56.3, -44.6, -26];
+RMIT1 = [20.1, -30.8, -20.6]; 
+RMIT2 = [28.6, -31.2, -22.8]; 
+RMIT3 = [40.6, -31.5, -24.9];
+RMIT4 = [51, -30.7, -30.8];
 
-ieeg_elAdd([RMIT1; RMIT2; RMIT3; RMIT4] ,'y',30)
+ieeg_elAdd([RMIT1; RMIT2; RMIT3; RMIT4] ,'g',30)
 
-RPIT1 = [31.5, -60.5, -18]; 
-RPIT2 = [40.1, -59, -22]; 
-RPIT3 = [49.8, -57.5, -18.5];
-RPIT4 = [56.2, -58, -19.5];
+RPIT1 = [13, -57.7, -6.8]; 
+RPIT2 = [24, -56.7, -9.5]; 
+RPIT3 = [34, -55.6, -18.8];
+RPIT4 = [44, -54.3, -22.2];
 
-ieeg_elAdd([RPIT1; RPIT2; RPIT3; RPIT4] ,'b',30)
+ieeg_elAdd([RPIT1; RPIT2; RPIT3; RPIT4] ,'y',30)
 
 % LPT 1-6
 % ieeg_viewLight(-90, 0) LL
 
 LPT1 = [-50.5, -78.6, 10.4];
-LPT2 = [-42, -68, 3.5];
+LPT2 = [-57, -70, 3.5];
 LPT3 = [-62.3, -61, -0.3];  
 LPT4 = [-65.2, -53.6, -3];     
 LPT5 = [-66, -45, -7.3];
 LPT6 = [-67, -36.5, -9.7];
 
-ieeg_elAdd([LPT1; LPT2; LPT3; LPT4; LPT5; LPT6] ,'y',30)
+ieeg_elAdd([LPT1; LPT2; LPT3; LPT4; LPT5; LPT6] ,'b',30)
 
 % LAT 1-4, LAIT 1-4, LPIT 1-4
 % ieeg_viewLight(-90, -90) LI
@@ -125,21 +125,21 @@ ieeg_elAdd([LPIT1; LPIT2; LPIT3; LPIT4] ,'y',30)
 % LF 1-4, LP 1-6
 % ieeg_viewLight(-90, 0) LL
 
-LF1 = [-59, 3.8, 36.1];
-LF2 = [-61, -0.9, 29];
-LF3 = [-65, -7.2, 22.9];
+LF1 = [-60.3, 0.6, 36.2];
+LF2 = [-62.3, -2.5, 29.9];
+LF3 = [-65.8, -8.1, 21.9];
 LF4 = [-66.8, -12, 15.1];
 
 ieeg_elAdd([LF1; LF2; LF3; LF4] ,'y',30)
 
-LP1 = [-46, -34.5, 53];
-LP2 = [-58, -40.5, 47.6];
-LP3 = [-59, -43.8, 41.7];
-LP4 = [-62, -46.1, 34.1];
-LP5 = [-63.5, -48.2, 26.7];
-LP6 = [-64.5, -49.8, 19.1];
+LP1 = [-45.9, -42.6, 61.9];
+LP2 = [-50.4, -43, 54.4];
+LP3 = [-56.5, -44.1, 48];
+LP4 = [-59.3, -46.2, 40.7];
+LP5 = [-62.5, -48.4, 31.7];
+LP6 = [-63.5, -50.2, 22.2];
 
-ieeg_elAdd([LP1; LP2; LP3; LP4; LP5; LP6] ,'y',30)
+ieeg_elAdd([LP1; LP2; LP3; LP4; LP5; LP6] ,'g',30)
 
 % LAT 1-6, LAST 1-4, LD 1-8, LMST 1-4, LPST 1-6
 % ieeg_viewLight(-180, -90) LI
@@ -190,14 +190,14 @@ ieeg_elAdd([LPST1; LPST2; LPST3; LPST4; LPST5; LPST6] ,'y',30)
 % LTO 1-8
 % ieeg_viewLight(90, 0) LL
 
-LTO1 = [0, -83, 18.5];
-LTO2 = [-2.5, -90.5, 18.5];
-LTO3 = [-9, -94, 18];
-LTO4 = [-13.7, -99.5, 17.7];
-LTO5 = [-21, -99.5, 16.5];
-LTO6 = [-26.8, -97.3, 14.9];
-LTO7 = [-32.8, -92.6, 12.4];
-LTO8 = [-38, -86.1, 9.5];
+LTO1 = [-37.4, -87, 2.4];
+LTO2 = [-26.5, -92.4, 1.8];
+LTO3 = [-22.8, -99, 4];
+LTO4 = [-15.6, -103, 5.1];
+LTO5 = [-5.1, -102.8, 6.7];
+LTO6 = [-2.7, -95.2, 8.3];
+LTO7 = [-0.1, -88, 10];
+LTO8 = [-1.4, -81.8, 11.7];
 
 ieeg_elAdd([LTO1; LTO2; LTO3; LTO4; LTO5; LTO6; LTO7; LTO8] ,'y',30)
 
@@ -270,13 +270,13 @@ ieeg_elAdd([RPST1; RPST2; RPST3; RPST4; RPST5; RPST6] ,'y',30)
 % RTO 1-8
 % ieeg_viewLight(90, 0) RL
 
-RTO1 = [1, -83, 18.5];
-RTO2 = [3.5, -90.5, 18.5];
-RTO3 = [9, -94, 18];
-RTO4 = [13.7, -99.5, 17.7];
-RTO5 = [21, -99.5, 16.5];
-RTO6 = [26.8, -97.3, 14.9];
-RTO7 = [32.8, -92.6, 12.4];
-RTO8 = [38, -86.1, 9.5];
+RTO1 = [8.5, -83, 4.2];
+RTO2 = [4.5, -90.5, 5.3];
+RTO3 = [8.5, -98.3, 6];
+RTO4 = [11, -103.5, 7];
+RTO5 = [21, -102, 6];
+RTO6 = [28, -97.3, 3];
+RTO7 = [32.8, -92.6, 2];
+RTO8 = [42, -88, 1];
 
-ieeg_elAdd([RTO1; RTO2; RTO3; RTO4; RTO5; RTO6; RTO7; RTO8] ,'y',30)
+ieeg_elAdd([RTO1; RTO2; RTO3; RTO4; RTO5; RTO6; RTO7; RTO8] ,'g',30)
